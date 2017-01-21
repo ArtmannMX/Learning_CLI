6d062a7a3af4f0cf37da30bbf1d919dfa0f15e90
ArtmannMX <artmx@protonmail.ch>
2017-01-20 23:12:23 -0600

    Create df.sh

---
+++ Hard_Disk_Management/Info/df.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Información acerca del espacio usado y libre de las particiones del hdd
+df    # Muestra el espacio en Bytes
+df -h    # Muestra el espacio en el rango de GB y MB el flag -h indica 'human readable'
