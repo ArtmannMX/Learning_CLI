72c218d9f39a8ad48a45f58962be2b7b2da0842d
ArtmannMX <artmx@protonmail.ch>
2017-04-08 02:46:54 -0500

    Create shutdown.sh

---
+++ shutdown.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Como apagar o reiniciar el sistema
+# Reincinar (Reboot)
+sudo reboot now
+sudo suhtdown -r now
+sudo shutdown +0
+# Apagar
+sudo shutdown
+sudo shutdown -P
+sudo shutdown -H
+sudo shutdown 
